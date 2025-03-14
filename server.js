23b172a33cb4efc62cd55e50dc04c08e84f1008d
Shrey <shreyproject4@gmail.com>
2025-03-14 19:19:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -52,6 +52,39 @@ Google Maps: https://www.google.com/maps?q=${location.latitude},${location.longi
   }
 });
 
+app.post("/send-audio-mail", async (req, res) => {
+  const { user, guardianEmails, audio } = req.body;
+
+  if (!guardianEmails || guardianEmails.length === 0) {
+    return res.status(400).json({ message: "No guardian emails provided." });
+  }
+
+  // Create transporter
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "lifeline8555@gmail.com",
+      pass: "gwhn trey sker wenp",
+    },
+  });
+
+  const mailOptions = {
+    from: "lifeline8555@gmail.com",
+    to: guardianEmails,
+    subject: "Urgent: Help",
+    text: ` ${user.name} is not feeling safe. Please reach out to them immediately.  
+They have sent an audio ${audio}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "Alert email sent successfully." });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ message: "Failed to send email." });
+  }
+});
+
 app.use("/user", userRoutes);
 // Start server
 const PORT = 3000;
