877836ed8b01addc9d9a23be95bc5c51d1a601da
Shrey <shreyproject4@gmail.com>
2025-03-25 00:03:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -140,11 +140,11 @@ app.post("/send-audio-mail", upload.single("file"), async (req, res) => {
       from: "lifeline8555@gmail.com",
       to: emails.join(", "), // Send to multiple recipients
       subject: "Urgent: Help",
-      text: `${userData.name} is not feeling safe. Please reach out to them immediately. Current Location:  
+      text: `${userData?.name} is not feeling safe. Please reach out to them immediately. Current Location:  
 Latitude: ${location.latitude}, Longitude: ${location.longitude}  
 
 Google Maps: https://www.google.com/maps?q=${location.latitude},${location.longitude} `,
-      html: `<p><strong>${user.name}</strong> is not feeling safe. Please reach out to them immediately.</p>
+      html: `<p><strong>${userData?.name}</strong> is not feeling safe. Please reach out to them immediately.</p>
          <p><strong>Current Location:</strong> Latitude: ${location.latitude}, Longitude: ${location.longitude}</p>
          <p><a href="https://www.google.com/maps?q=${location.latitude},${location.longitude}" target="_blank">
          <strong>Click here to view location in Google Maps</strong></a></p>`,
